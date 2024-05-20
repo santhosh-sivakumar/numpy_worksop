97929ec73fb2413daeadb892e8f76bad988b3ffc
Santhosh Sivakumar <santhosh.s2023ai-ds@sece.ac.in>
2024-05-20 14:19:30 +0530

    Added Answer to Question 3

--- question3.py
+++ question3.py
@@ -1,1 +1,10 @@
 #write a program to find the factorial of a nummber
+
+def fact(n):
+  if n == 1 or n == 0:
+      return n
+  else:
+      return n * fact( n - 1 )
+
+a = int( input("Enter a Number: "))
+print("The Factorial: ", fact(a) )
