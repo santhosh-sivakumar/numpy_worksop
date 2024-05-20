684aa364c4e784cfebb7857a3e872d4c9a04cab1
Santhosh Sivakumar <santhosh.s2023ai-ds@sece.ac.in>
2024-05-20 14:15:12 +0530

    Added Answer to Question 2

--- question2.py
+++ question2.py
@@ -1,1 +1,8 @@
 # find if the given number is a palindrome or not?
+
+a = input("Enter a Number: ")
+
+if( a == a[::-1] ):
+  print("Given Input is Palindrome")
+else:
+  print("Given Input is Not a Palindrome")
