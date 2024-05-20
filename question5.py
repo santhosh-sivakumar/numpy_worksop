0c952f892d0b08af48314148a3bfca78feead76f
Santhosh Sivakumar <santhosh.s2023ai-ds@sece.ac.in>
2024-05-20 14:31:48 +0530

    Added Answer to Question 5

--- question5.py
+++ question5.py
@@ -1,1 +1,13 @@
 #write a program to find if the given number is prime no or not
+
+def prime(n):
+    for i in range( 2, n//2 + 1 ):
+        if ( n % i == 0):
+            return False
+    return True
+
+a = int( input("Enter a Number: "))
+if( prime(a) ):
+    print("Number is Prime")
+else:
+    print("Number is not Prime")
