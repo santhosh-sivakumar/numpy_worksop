083e0e9d4e337e9ebf63ae3c78c72138d16a7041
Santhosh Sivakumar <santhosh.s2023ai-ds@sece.ac.in>
2024-05-20 14:24:09 +0530

    Added Answer to Question 4

--- question4.py
+++ question4.py
@@ -1,1 +1,11 @@
 #write a program to find the sum of digits of a given number'
+
+def sod( n ):
+  sum = 0
+  while( n > 0 ):
+    sum += ( n % 10 )
+    n = ( n // 10 )
+  return sum
+
+a = int( input("Enter a Number: "))
+print("The sum of the digits: ", sod(a))
